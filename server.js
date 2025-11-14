13f51e31b676ee5cb830292afdc30ed1b23cc205
ganggang0511-pixel <ganggang0511@gmail.com>
2025-11-14 16:50:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Sevalla Node! 部署成功！</h1><p>端口: ' + port + '</p>');
+  res.send('<h1>Sevalla 部署成功！</h1>');
 });
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`运行在端口 ${port}`);
 });
